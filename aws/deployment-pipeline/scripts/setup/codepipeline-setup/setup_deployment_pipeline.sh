bfbe8430f1bea168e9baed257ed42baa52b7bd0d
rubenpulido <ruben.pulido@liferay.com>
2017-08-11 11:06:46 +0200

    PULPO-19 Revert commented lines

--- aws/deployment-pipeline/scripts/setup/codepipeline-setup/setup_deployment_pipeline.sh
+++ aws/deployment-pipeline/scripts/setup/codepipeline-setup/setup_deployment_pipeline.sh
@@ -20,23 +20,23 @@ set +o allexport
 
 set -x
 
-#prereqs/s3/create_bucket.sh \
-#	-n ${BUCKET_NAME}
-#
-#prereqs/s3/put_object.sh \
-#	-n ${BUCKET_NAME} \
-#	-k ${DOCKER_CREDENTIALS_KEY} \
-#	-c ${CONFIG_DIR}
-#
-#prereqs/iam/create_role.sh \
-#	-r ${CODEBUILD_SERVICE_ROLE_NAME}
-#
-#PATH_TO_CODEBUILD_POLICY_DOCUMENT=${CONFIG_DIR}/prereqs/iam/codebuild-role-policy.json
-#
-#prereqs/iam/put_role_policy.sh \
-#	-r ${CODEBUILD_SERVICE_ROLE_NAME} \
-#	-p ${CODEBUILD_SERVICE_ROLE_POLICY} \
-#	-d ${PATH_TO_CODEBUILD_POLICY_DOCUMENT}
+prereqs/s3/create_bucket.sh \
+	-n ${BUCKET_NAME}
+
+prereqs/s3/put_object.sh \
+	-n ${BUCKET_NAME} \
+	-k ${DOCKER_CREDENTIALS_KEY} \
+	-c ${CONFIG_DIR}
+
+prereqs/iam/create_role.sh \
+	-r ${CODEBUILD_SERVICE_ROLE_NAME}
+
+PATH_TO_CODEBUILD_POLICY_DOCUMENT=${CONFIG_DIR}/prereqs/iam/codebuild-role-policy.json
+
+prereqs/iam/put_role_policy.sh \
+	-r ${CODEBUILD_SERVICE_ROLE_NAME} \
+	-p ${CODEBUILD_SERVICE_ROLE_POLICY} \
+	-d ${PATH_TO_CODEBUILD_POLICY_DOCUMENT}
 
 PATH_TO_CODEBUILD_POLICY_DOCUMENT=${CONFIG_DIR}/prereqs/iam/eb-ec2-role-policy.json
 
@@ -45,21 +45,21 @@ prereqs/iam/put_role_policy.sh \
 	-p ${EB_EC2_SERVICE_ROLE_POLICY} \
 	-d ${PATH_TO_CODEBUILD_POLICY_DOCUMENT}
 
-#prereqs/eb/create_eb_environments.sh \
-#	-n ${APPLICATION_NAME} \
-#	-e ${ENVIRONMENT_SUFFIXES} \
-#	-o ${DEPLOYMENT_ARTIFACTS_ORG} \
-#	-r ${DEPLOYMENT_ARTIFACTS_REPO} \
-#	-b ${DEPLOYMENT_ARTIFACTS_BRANCH} \
-#	-i ${INSTANCE_TYPE} \
-#	-g ${REGION} \
-#	-c ${CONFIG_DIR}
-#
-#prereqs/codebuild/create_codebuild_projects.sh \
-#	-n ${APPLICATION_NAME} \
-#	-e ${ENVIRONMENT_SUFFIXES} \
-#	-s ${CODEBUILD_SERVICE_ROLE_ARN} \
-#	-c ${CONFIG_DIR}
-#
-#codepipeline/create_deployment_pipeline.sh \
-#	-c ${CONFIG_DIR}
+prereqs/eb/create_eb_environments.sh \
+	-n ${APPLICATION_NAME} \
+	-e ${ENVIRONMENT_SUFFIXES} \
+	-o ${DEPLOYMENT_ARTIFACTS_ORG} \
+	-r ${DEPLOYMENT_ARTIFACTS_REPO} \
+	-b ${DEPLOYMENT_ARTIFACTS_BRANCH} \
+	-i ${INSTANCE_TYPE} \
+	-g ${REGION} \
+	-c ${CONFIG_DIR}
+
+prereqs/codebuild/create_codebuild_projects.sh \
+	-n ${APPLICATION_NAME} \
+	-e ${ENVIRONMENT_SUFFIXES} \
+	-s ${CODEBUILD_SERVICE_ROLE_ARN} \
+	-c ${CONFIG_DIR}
+
+codepipeline/create_deployment_pipeline.sh \
+	-c ${CONFIG_DIR}
