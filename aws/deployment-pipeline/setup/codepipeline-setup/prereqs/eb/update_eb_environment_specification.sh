797cc9611651df8223a3b6a23479e7bfce020976
rubenpulido <ruben.pulido@liferay.com>
2017-09-15 00:33:09 +0200

    PULPO-19 Update EB environment specification before creating EB environment for the first time

--- aws/deployment-pipeline/setup/codepipeline-setup/prereqs/eb/update_eb_environment_specification.sh
+++ aws/deployment-pipeline/setup/codepipeline-setup/prereqs/eb/update_eb_environment_specification.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-while getopts "c:" OPT; do
+while getopts "c:p:" OPT; do
   case ${OPT} in
     c) CONFIG_DIR="${OPTARG}"
+    ;;
+    p) PATH_TO_EB_DOCKER_JSON_FILE="${OPTARG}"
     ;;
     \?) echo "Invalid option -${OPTARG}" >&2
     exit 1
@@ -20,11 +22,9 @@ set +o allexport
 
 set -x
 
-PATH_TO_EB_DOCKER_JSON_FILE="${CONFIG_DIR}/codepipeline/deployment-pipeline.json"
-
 # Configure authentication bucket name
 
-jq '.authentication.bucket = "codepipeline-'${APPLICATION_NAME}'"' ${PATH_TO_EB_DOCKER_JSON_FILE}|sponge ${PATH_TO_EB_DOCKER_JSON_FILE}
+jq '.authentication.bucket = "'${BUCKET_NAME}'"' ${PATH_TO_EB_DOCKER_JSON_FILE}|sponge ${PATH_TO_EB_DOCKER_JSON_FILE}
 
 # Configure docker image
 
