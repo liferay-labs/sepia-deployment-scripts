1d2fb1106f221961d71701dd24d52c97f2d73a4d
rubenpulido <ruben.pulido@liferay.com>
2017-09-14 23:47:40 +0200

    PULPO-19 Install EB CLI with sudo

--- aws/deployment-pipeline/setup/codepipeline-setup/prereqs/cli/setup_aws_cli_bundle.sh
+++ aws/deployment-pipeline/setup/codepipeline-setup/prereqs/cli/setup_aws_cli_bundle.sh
@@ -3,4 +3,4 @@
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
-pip3 install awsebcli --upgrade
+sudo pip3 install awsebcli --upgrade
