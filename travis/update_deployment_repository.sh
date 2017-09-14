b3c00acd1364ea4059224e3aa451cc7e059beff6
Eduardo Garcia <eduardo.garcia@liferay.com>
2017-09-14 10:37:42 +0200

    PULPO-19 Remove unnecessary logic

--- travis/update_deployment_repository.sh
+++ travis/update_deployment_repository.sh
@@ -1,76 +1,10 @@
 #!/usr/bin/env bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-PATH_TO_ENV_FILE="${DIR}/update_deployment_repository.env"
-
-while getopts ":p:c:v:i:o:" opt; do
-  case ${opt} in
-    p) PATH_TO_ENV_FILE="${OPTARG}"
-    ;;
-    c) componentName="${OPTARG}"
-    ;;
-    i) dockerImageName="${OPTARG}"
-    ;;
-    v) dockerImageVersion="${OPTARG}"
-    ;;
-    o) dockerOrg="${OPTARG}"
-    ;;
-    \?) echo "Invalid option -${OPTARG}" >&2
-    exit 1
-    ;;
-  esac
-done
-
-. ${PATH_TO_ENV_FILE}
-
-printf "Parameters: \n"
-printf "componentName: %s\n" "${componentName}"
-printf "dockerImageName: %s\n" "${dockerImageName}"
-printf "dockerImageVersion: %s\n" "${dockerImageVersion}"
-printf "dockerOrg: %s\n" "${dockerOrg}"
-printf "\n"
-
-printf "Env variables: \n"
-printf "COMPONENT_NAME: %s\n" "${COMPONENT_NAME}"
-printf "DOCKER_IMAGE_NAME: %s\n" "${DOCKER_IMAGE_NAME}"
-printf "DOCKER_IMAGE_VERSION: %s\n" "${DOCKER_IMAGE_VERSION}"
-printf "DOCKER_ORG: %s\n" "${DOCKER_ORG}"
-printf "\n"
-
-# Set COMPONENT_NAME variable if not already set and parameter is present
-if [ -z "${COMPONENT_NAME}" ]; then
-    if [[ ! -z "${componentName}" ]]; then
-        COMPONENT_NAME=${componentName}
-    fi
-fi
-
-# Set DOCKER_IMAGE_NAME variable if not already set and parameter is present
-if [ -z "${DOCKER_IMAGE_NAME}" ]; then
-    if [ -z "${dockerImageName}" ]; then
-        DOCKER_IMAGE_NAME=${dockerImageName}
-    fi
-fi
-
-# Set DOCKER_ORG variable if not already set and parameter is present
-if [ -z "${DOCKER_ORG}" ]; then
-    if [ -z "${dockerOrg}" ]; then
-        DOCKER_ORG=${dockerOrg}
-    fi
-fi
-
-# Get explicit tag for docker image from images built locally if not already set
-if [ -z "${DOCKER_IMAGE_VERSION}" ]; then
-    if [ -z "${dockerImageVersion}" ]; then
-        DOCKER_IMAGE_VERSION=$(docker images ${DOCKER_ORG}/${DOCKER_IMAGE_NAME} --format "{{.Tag}}" | grep ".*T.*Z")
-    else
-        DOCKER_IMAGE_VERSION=${dockerImageVersion}
-    fi
-fi
-
-
 printf "Values used: \n"
-printf "COMPONENT_NAME: %s\n" "${COMPONENT_NAME}"
+printf "COMMIT_MESSAGE_PREFIX: %s\n" "${COMMIT_MESSAGE_PREFIX}"
+printf "DEPLOYMENT_ARTIFACTS_BRANCH: %s\n" "${DEPLOYMENT_ARTIFACTS_BRANCH}"
+printf "DEPLOYMENT_ARTIFACTS_ORG: %s\n" "${DEPLOYMENT_ARTIFACTS_ORG}"
+printf "DEPLOYMENT_ARTIFACTS_REPO: %s\n" "${DEPLOYMENT_ARTIFACTS_REPO}"
 printf "DOCKER_IMAGE_NAME: %s\n" "${DOCKER_IMAGE_NAME}"
 printf "DOCKER_IMAGE_VERSION: %s\n" "${DOCKER_IMAGE_VERSION}"
 printf "DOCKER_ORG: %s\n" "${DOCKER_ORG}"
@@ -82,9 +16,9 @@ set -x
 # Clone deployment repository
 cd ${TRAVIS_BUILD_DIR}/..
 
-git clone --depth=50 --branch=${DEPLOYMENT_REPO_BRANCH} git@github.com:${DEPLOYMENT_REPO_ORG}/${DEPLOYMENT_REPO_NAME}.git
+git clone --depth=50 --branch=${DEPLOYMENT_ARTIFACTS_BRANCH} git@github.com:${DEPLOYMENT_ARTIFACTS_ORG}/${DEPLOYMENT_ARTIFACTS_REPO}.git
 
-DEPLOYMENT_REPO_DIR=${TRAVIS_BUILD_DIR}/../${DEPLOYMENT_REPO_NAME}
+DEPLOYMENT_REPO_DIR=${TRAVIS_BUILD_DIR}/../${DEPLOYMENT_ARTIFACTS_REPO}
 
 cd ${DEPLOYMENT_REPO_DIR}
 
@@ -94,6 +28,6 @@ jq '.containerDefinitions[0].image = "'${DOCKER_ORG}'/'${DOCKER_IMAGE_NAME}':'${
 
 git add ${DEPLOYMENT_REPO_DIR}/Dockerrun.aws.json
 
-git commit -m "${DEPLOYMENT_JIRA_ISSUE} DOCKER_IMAGE_VERSION: ${DOCKER_IMAGE_VERSION}. Trigger TRAVIS_COMMIT: ${TRAVIS_COMMIT}. TRAVIS_BUILD_ID=${TRAVIS_BUILD_ID}"
+git commit -m "${COMMIT_MESSAGE_PREFIX} DOCKER_IMAGE_VERSION: ${DOCKER_IMAGE_VERSION}. Trigger TRAVIS_COMMIT: ${TRAVIS_COMMIT}. TRAVIS_BUILD_ID=${TRAVIS_BUILD_ID}"
 
 git push
