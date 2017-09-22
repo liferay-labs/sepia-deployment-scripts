06121a16cad23f78dd2fbc9876b52f8f8fff2f0d
rubenpulido <ruben.pulido@liferay.com>
2017-09-22 16:21:43 +0200

    PULPO-19 List pipelines and parse output to determine if pipeline exists

--- aws/deployment-pipeline/check/codepipeline/check_deployment_pipeline_exists.sh
+++ aws/deployment-pipeline/check/codepipeline/check_deployment_pipeline_exists.sh
@@ -16,23 +16,10 @@ done
 
 PIPELINE_NAME=${APPLICATION_NAME}-deployment-pipeline
 
-(
-	aws codepipeline get-pipeline --name ${PIPELINE_NAME}
-)
+aws codepipeline list-pipelines |jq -r '.pipelines[].name' |grep -q "^${PIPELINE_NAME}$"
 
-if [ $? = 255 ]
-then
-  echo "Codepipeline ${PIPELINE_NAME} does not exist"
-  exit 0
-fi
-
-if [ $? = 0 ]
-then
+if [ $? -eq 0 ]; then
   echo "Codepipeline ${PIPELINE_NAME} exists"
   echo "Creating file ${PIPELINE_EXISTS_FILE}"
   touch ${PIPELINE_EXISTS_FILE}
-  exit 0
 fi
-
-echo "Could not determine if codepipeline ${PIPELINE_NAME} exists"
-exit 1
