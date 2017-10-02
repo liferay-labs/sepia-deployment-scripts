de917b812bea4245736522e1ac1bb75466946c9f
rubenpulido <ruben.pulido@liferay.com>
2017-10-02 15:33:54 +0200

    PULPO-19 Print aws and eb CLI versions

--- aws/deployment-pipeline/setup/codepipeline-setup/prereqs/cli/setup_aws_cli_bundle.sh
+++ aws/deployment-pipeline/setup/codepipeline-setup/prereqs/cli/setup_aws_cli_bundle.sh
@@ -4,3 +4,6 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 sudo pip3 install awsebcli --upgrade
+
+aws --version
+eb --version
