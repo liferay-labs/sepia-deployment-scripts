a4af146047f74d2fdbc792b1806a28d409b50155
Cristina González Castellano <cristina.gonzalez@liferay.com>
2017-12-19 14:03:25 +0100

    Revert "PULPO-19 Check if cname is available only once"
    This reverts commit a4444d6ec18b993dd8eaf2ecc9ad00ee22ed6879.

--- aws/deployment-pipeline/setup/codepipeline-setup/prereqs/eb/create_eb_environment.sh
+++ aws/deployment-pipeline/setup/codepipeline-setup/prereqs/eb/create_eb_environment.sh
@@ -28,9 +28,7 @@ set -x
 
 CNAME="${APPLICATION_NAME}-${ENVIRONMENT_SUFFIX}"
 
-echo "Make sure cname is available: ${CNAME}"
-
-${DIR}/wait_until_cname_is_available.sh -c ${CNAME}
+bash ./wait_until_cname_is_available.sh -c ${CNAME}
 
 echo "Read environment variables for environment"
 
@@ -41,6 +39,10 @@ echo "EB_ENV_VARS: ${EB_ENV_VARS}"
 PREVIOUS_DIR=$(pwd)
 
 cd ${PATH_TO_DEPLOYMENT_ARTIFACTS_REPO}
+
+echo "Make sure cname is available: ${CNAME}"
+
+${DIR}/wait_until_cname_is_available.sh -c ${CNAME}
 
 echo "Create eb environment from directory: ${PATH_TO_DEPLOYMENT_ARTIFACTS_REPO}"
 
