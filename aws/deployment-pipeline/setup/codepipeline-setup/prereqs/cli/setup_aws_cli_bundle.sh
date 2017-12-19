93bbf57f2a6ac26c40d529f9f64699f188dbb50e
Cristina González Castellano <cristina.gonzalez@liferay.com>
2017-12-19 14:02:38 +0100

    Revert "PULPO-19 Print aws and eb CLI versions"
    This reverts commit de917b812bea4245736522e1ac1bb75466946c9f.

--- aws/deployment-pipeline/setup/codepipeline-setup/prereqs/cli/setup_aws_cli_bundle.sh
+++ aws/deployment-pipeline/setup/codepipeline-setup/prereqs/cli/setup_aws_cli_bundle.sh
@@ -4,6 +4,3 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 sudo pip3 install awsebcli --upgrade
-
-aws --version
-eb --version
